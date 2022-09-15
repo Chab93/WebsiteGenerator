32797f6fa4be444b6edb80a8801dbdd29f106e41
Chab93 <vikcrille93@gmail.com>
2022-09-15 15:16:52 +0200

    Added branch for StyledWebsite

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,28 +14,45 @@ namespace WebsiteGenerator
             string nameOfClass = "ITHS .NET 2022";
             string[] messages = { "Nu ska vi lära oss att programmera", "Testmeddelande", "Meddelande 3" };
             string[] classes = { "   C#", "daTAbaser", "WebbuTVeCkling  ", "clean Code      " };
+            string color = "red";
 
-            GenerateWebsite website1 = new GenerateWebsite(nameOfClass, messages, classes);
+            GenerateWebsite website1 = new GenerateWebsite();
+            website1.PrintWebpage(nameOfClass, messages, classes);
+
+            Console.WriteLine("\n\n");
+
+            StyledWebsite website2 = new StyledWebsite();
+            website2.PrintWebpage(nameOfClass, messages, classes, color);
 
             Console.ReadLine();
         }
 
+        class StyledWebsite : GenerateWebsite
+        {
+            override protected void ClassAndMessage(string className, string[] messages, string[] classes, string color)
+            {
+                string style = $"p {{ color: {color}; }}";
+
+                Console.WriteLine($"<head>\n<style>\n{style}\n</style>\n</head>");
+
+                Console.WriteLine($"<body>\n<h1>Välkomna {className}!</h1>");
+
+                for (int i = 0; i < messages.Length; i++)
+                {
+                    Console.WriteLine($"<p><b>Meddelande {i + 1}:</b> {messages[i]}</p>");
+                }
+
+                Console.WriteLine("<main>");
+
+                foreach (string c in classes)
+                {
+                    Console.WriteLine($"<p>Kurs om {c.Trim().Substring(0, 1).ToUpper()}{c.Trim().Substring(1).ToLower()}</p>");
+                }
+            }
+        }
         class GenerateWebsite
         {
-            string className;
-            string[] messages;
-            string[] classes;
-
-            public GenerateWebsite(string className, string[] messages, string[] classes)
-            {
-                this.className = className;
-                this.messages = messages;
-                this.classes = classes;
-
-                PrintWebpage(className, messages, classes);
-            }
-
-            static void PrintWebpage(string className, string[] messages, string[] classes)
+            public void PrintWebpage(string className, string[] messages, string[] classes)
             {
                 Start();
 
@@ -43,12 +61,21 @@ namespace WebsiteGenerator
                 End();
             }
 
-            static void Start()
+            public void PrintWebpage(string className, string[] messages, string[] classes, string color)
+            {
+                Start();
+
+                ClassAndMessage(className, messages, classes, color);
+
+                End();
+            }
+
+            void Start()
             {
                 Console.WriteLine("<!DOCTYPE html>\n<html>");
             }
 
-            static void ClassAndMessage(string className, string[] messages, string[] classes)
+            void ClassAndMessage(string className, string[] messages, string[] classes)
             {
 
                 Console.WriteLine($"<body>\n<h1>Välkomna {className}!</h1>");
@@ -66,10 +93,15 @@ namespace WebsiteGenerator
                 }
             }
 
-            static void End()
+            virtual protected void ClassAndMessage(string className, string[] messages, string[] classes, string color)
+            {
+            }
+
+            void End()
             {
                 Console.WriteLine("</main>\n</body>\n</html>");
             }
         }
+
     }
 }
